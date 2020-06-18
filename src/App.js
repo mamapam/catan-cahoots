a438e96050bcc6a88308f3ebd48f193444e1d922
mamapam <mamapam.dev@gmail.com>
2020-06-18 14:51:39 -0400

    Setup grid system for app and setup header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Grid } from '@material-ui/core';
+
+import Header from './components/Header/Header';
 
 function App() {
   return (
-    <div>
-      Catan Cahoots!
-    </div>
+    <Grid container direction="column">
+
+      <Grid item>
+        <Header />
+      </Grid>
+
+      <Grid item container>
+        <Grid item xs={false} md={2} />
+        <Grid item sm={12} md={8} >
+          content will go here
+        </Grid>
+        <Grid item xs={false} md={2} />
+      </Grid>
+
+    </Grid>
   );
 }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,26 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography, IconButton } from '@material-ui/core';
+import { AppBar, Toolbar, Typography, IconButton, Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 
+const useStyles = makeStyles(() => ({
+  typographyStyle: {
+    flex: 1
+  }
+}));
+
 const Header = () => {
+  const classes = useStyles();
+
   return(
     <AppBar position="static">
       <Toolbar>
-        <Typography variant="h1">Catan Cahoots!</Typography>
+        <Grid item md={1} />
+        <Typography className={classes.typographyStyle}>Catan Cahoots!</Typography>
         <IconButton>
           <AccountCircleIcon />
         </IconButton>
+        <Grid item md={1} />
       </Toolbar>
     </AppBar>
   );
